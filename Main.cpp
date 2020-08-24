c1841102cde2b3668bde90a6e9ab38a15861af18
/JeanCarlos <64989232+JeanCarlos911@users.noreply.github.com>
2020-08-24 17:22:51 -0500

    Update Main.cpp
    Los anteriores cambios fueron realizados a través del celular, son un boceto de la app y tiene error en sintaxis, luego se corregirán una vez tenga contacto con mi pc. Está incompleto porque ya se va a apagar :c

--- Main.cpp
+++ Main.cpp
@@ -21,42 +21,78 @@ int main(){
 
     cout << "La cantidad de minas es: " << minas << endl;
     
-    //COLOCA MINAS: el cero es vacio, el menos uno es mina
+    //LLENA LA MATRIZ CON -10 (espacios vacios que no se muestran)
     for(int i=0; i<size; i++){
         for(int j=0; j<size; j++){
-            if(minas>0){
-                int tipoCasilla = rand() % 2 - 1;
-                if(tipoCasilla == -1){
-                    minas --;
-                }
-                matriz[i][j] = tipoCasilla;
-            }else{
-                matriz[i][j] = 0;
-            }
+            matriz[i][j] = -10;
         }
     }
 
-    //IMPRIMIR TABLERO
-    for(int i=0; i<size; i++){
-        //parte superior de la linea
-        for(int j=0; j<size; j++){
-            printf("%s",(i==0)?"+---":"|---");
+    //COLOCA MINAS: el -10 es vacio y no muestra, el -11 es mina y no muestra
+    while(minas>0){
+        int valorA = rand() % size;
+        int valorB = rand() % size;
+        if(matriz[valorA][valorB] != -11){
+            matriz[valorA][valorB] = -11;
+            minas--;
         }
-        printf("%s",(i==0)?"+\n":"|\n");
+    }
 
-        //linea
-        for(int j=0; j<size; j++){
-            printf("%s%3d", "|", matriz[i][j]);
-        }
-        cout << "|" << endl;
+    //EMPIEZA EL JUEGO
+    String estado = "continuar";
 
-        //parte inferior
-        if(i==size-1){
+    while(true){
+        //MUESTRA TABLERO 
+        for(int i=0; i<size; i++){
+            //parte superior de la linea
             for(int j=0; j<size; j++){
-                printf("%s","+---");
+                printf("%s",(i==0)?"+---":"|---");
             }
-            printf("%s","+\n");
-        }
-    }
+            printf("%s",(i==0)?"+\n":"|\n");
 
+            //linea
+            for(int j=0; j<size; j++){
+                String valor= " ";
+                switch(matriz[i][j]){
+                    case 12:
+                        valor = "X";//casilla marcada
+                        break;
+                    case 11:
+                        valor = "#";//piso mina
+                        break;
+                }
+                if(matriz[i][j]<11 && matriz[i][j]>=0){
+                    valor = String.format("%d", matriz[i][j]);
+                }
+                printf("%s%3s", "|", valor);
+            }
+            cout << "|" << endl;
+
+            //parte inferior
+            if(i==size-1){
+                for(int j=0; j<size; j++){
+                    printf("%s","+---");
+                }
+                printf("%s","+\n");
+            }
+        }
+   
+        //PIDE ENTRADA
+        //cout << "Ingrese fila de la casilla" << ends;
+        //ingrese columna
+        //desea pisar(1),  marcar(2) o demarcar(3)?
+
+        //SI PISA UNA CASILLA DE CERO, MUESTRA LAS OTRAS CASILLAS
+
+        //SI PISA UNA MINA PIERDE Y SALE
+
+        //SI PISA UN NUMERO LO MUESTRA
+
+        //SI TODAS LAS CASILLAS SON MAYORES QUE CERO GANA Y SALE
+
+        //SI MARCA CASILLA, GUARDA SU VALOR ORIGINAL Y MUESTRA X
+    
+        //SI DESMARCA RECUPERA SU VALOR ORIGINAL NO MUESTRA
+    }
+    
 }
