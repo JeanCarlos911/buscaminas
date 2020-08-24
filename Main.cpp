0ff968551798b1386c82168045f8a14e2e4bccd4
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-08-24 10:17:28 -0500

    added problem

---
+++ Main.cpp
@@ -0,0 +1,50 @@
+# include <iostream>
+
+using namespace std;
+
+void imprimirTablero(int size, int **matriz){
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            cout << "|" << matriz[i][j] << ends;
+        }
+        cout << "|" << endl;
+    }
+}
+
+int main(){
+    int size;
+
+    cout << "\n"
+            "      ____                                  _                 \n"
+            "     / __ )__  ________________ _____ ___  (_)___  ____ ______\n"
+            "    / __  / / / / ___/ ___/ __ `/ __ `__ \\/ / __ \\/ __ `/ ___/\n"
+            "   / /_/ / /_/ (__  ) /__/ /_/ / / / / / / / / / / /_/ (__  ) \n"
+            "  /_____/\\__,_/____/\\___/\\__,_/_/ /_/ /_/_/_/ /_/\\__,_/____/  \n"
+            "                                                              \n" << endl;
+
+    cout << "- Cuanto mide cada lado de la matriz?\n  $ " << ends;
+    cin >> size;
+
+    int matriz[size][size];
+    int minas = 3 * size;
+
+    cout << "La cantidad de minas es: " << minas << endl;
+    
+    //COLOCA MINAS: el cero es vacio, el menos uno es mina
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            if(minas>0){
+                int tipoCasilla = rand() % 2 - 1;
+                if(tipoCasilla == 1){
+                    minas --;
+                }
+                matriz[i][j] = tipoCasilla;
+            }else{
+                matriz[i][j] = 0;
+            }
+        }
+    }
+
+    imprimirTablero(size, matriz);
+
+}
